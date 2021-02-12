a0d8f5eb08cf3e29db762ec290befb2062651fb6
TruKat <kati.k220@gmail.com>
2021-02-11 22:57:53 -0700

    deploying burger app

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.JAWSDB_URL || {
     host: "localhost",
     port: 3306,
     user: "root",
